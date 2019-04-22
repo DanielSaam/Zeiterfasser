f8ce8e8d5635b4abf0470de7aa76cdf851b479fd
Daniel Maas <dan.maas93@gmail.com>
2019-04-22 12:36:16 +0200

    Dropdownmenü hinzugefügt

--- app/src/main/java/de/fowli/zeiterfasser/MainActivity.java
+++ app/src/main/java/de/fowli/zeiterfasser/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -60,6 +64,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Spinner 1
+        final Spinner spinnerTatigkeit = (Spinner) findViewById(R.id.spinnerTatigkeit);
+        final ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.spinner_tatigkeit, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTatigkeit.setAdapter(adapter1);
+
+
+        // Spinner 2
+        final Spinner spinnerTatigkeitErweitert = (Spinner) findViewById(R.id.spinnerTatigkeitErweitert);
+        final ArrayAdapter<CharSequence> adapterArbeit = ArrayAdapter.createFromResource(this, R.array.spinner_tatigkeitArbeit, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterPrivat = ArrayAdapter.createFromResource(this, R.array.spinner_tatigkeitPrivat, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterGemeinnutzig = ArrayAdapter.createFromResource(this, R.array.spinner_tatigkeitGemeinnutzig, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterHaushalt = ArrayAdapter.createFromResource(this, R.array.spinner_tatigkeitHaushalt, android.R.layout.simple_spinner_item);
+
+        adapterArbeit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterPrivat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterGemeinnutzig.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterHaushalt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Der Spinner wird überwacht
+        spinnerTatigkeit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        spinnerTatigkeitErweitert.setAdapter(adapterArbeit);
+                        break;
+                    case 1:
+                        spinnerTatigkeitErweitert.setAdapter(adapterPrivat);
+                        break;
+                    case 2:
+                        spinnerTatigkeitErweitert.setAdapter(adapterGemeinnutzig);
+                        break;
+                    case 3:
+                        spinnerTatigkeitErweitert.setAdapter(adapterHaushalt);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     // Datum und die Zeit initialisieren
@@ -96,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
 
         String delimiter = ":"; /* Das Trennzeichen */
 
+        // Die Strings splitten
         arrayZeitStart = stringZeitStartSplit.split(delimiter);
         arrayZeitEnde = stringZeitEndeSplit.split(delimiter);
 
